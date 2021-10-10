b3d138119074790bc026671be5063035e248319e
SamuelDear <samuel_dear@hotmail.com>
2021-10-10 08:56:44 -0400

    Add Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,78 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import LOGO_TRAUMAPP from '../public/images/logotraumapp.png';
+import classes from '../styles/components/Header.module.css';
+
 const Header = () => {
+  const [activateAnimation, setActivateAnimation] = useState(false);
+
+  const activeMenu = () => {
+    setTimeout(() => {
+      setActivateAnimation(!activateAnimation);
+    }, 100);
+  };
+
   return (
     <header>
-      <p>Hola</p>
+      <div id="headerHome" className={classes.headerBox}>
+        <div className={classes.logoBox}>
+          <Image
+            src={LOGO_TRAUMAPP}
+            height="30px"
+            width="30px"
+            classes={classes.logoTraumappHeader}
+            title="TraumApp"
+            alt="TraumApp"
+            id="logoTraumApp1"
+          />
+          <h1 className={classes.logoName}>TraumApp</h1>
+        </div>
+        <nav className={`${classes.navBox} ${classes.showDesktop}`}>
+          <span className={classes.buttonNav}>About Us</span>
+          <span className={classes.buttonNav}>Services</span>
+          <span className={classes.buttonNav}>Contact</span>
+          <div className={`${classes.btnLoginBox} ${classes.showDesktop}`}>
+            <button className={classes.btnLogin} type="button">
+              Login
+            </button>
+          </div>
+        </nav>
+        <div className={classes.showMobile}>
+          <div
+            className={classes.burguer}
+            onClick={() => {
+              activeMenu();
+            }}>
+            <span className={classes.line}></span>
+            <span className={classes.line}></span>
+            <span className={classes.line}></span>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${classes.showMobile} ${classes.listFloat} ${
+          activateAnimation ? classes.listActive : ''
+        }`}>
+        <nav className={classes.navBox}>
+          <span
+            className={`${classes.buttonNav} ${classes.buttonNavMobile} ${classes.firstItemMobile}`}>
+            About Us
+          </span>
+          <span className={classes.separationItem}></span>
+          <span className={`${classes.buttonNav} ${classes.buttonNavMobile}`}>
+            Services
+          </span>
+          <span className={classes.separationItem}></span>
+          <span className={`${classes.buttonNav} ${classes.buttonNavMobile}`}>
+            Contact
+          </span>
+          <span className={classes.separationItem}></span>
+          <div className={`${classes.buttonNav} ${classes.buttonNavMobile}`}>
+            Login
+          </div>
+          <span className={classes.separationItem}></span>
+        </nav>
+      </div>
     </header>
   );
 };
